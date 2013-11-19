ec9e8e10b987b885334753b093355956ca15c4e7
Phillip Lamplugh <phillip.lamplugh@gmail.com>
2013-11-19 17:34:47 -0500

    comments out user attrs

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
-  attr_accessible :email, :password_digest, :password, :password_confirmation
-  has_secure_password
-  validates :password, length: { in: 8..16 }
-  validates :email, presence: true
+  # attr_accessible :email, :password_digest, :password, :password_confirmation
+  # has_secure_password
+  # validates :password, length: { in: 8..16 }
+  # validates :email, presence: true
 end
